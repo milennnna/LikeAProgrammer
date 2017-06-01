e19a99e14cb3c10c7faa4fba14246da4e2f11957
Milena Cakovic <milena.cakovic@namics.com>
2017-06-01 17:45:03 +0200

    Add shield rotation

--- LikeAProgrammer/Assets/scripts/Shield.cs
+++ LikeAProgrammer/Assets/scripts/Shield.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour {
 
+	public float speed = 4;
+	public Rigidbody2D rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Rotate(0.0f, 0.0f, -10.0f);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate(0.0f, 0.0f, 10.0f);
+		}
+	}
+
+	void FixedUpdate() {
+		//rigidBody.MoveRotation (rigidBody.rotation + speed * Time.fixedDeltaTime);
 	}
 }
