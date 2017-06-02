dbaf5b3f29c95a64bae6723121642a451111f5e4
Zeljko Ilic <zeljko.ilic@namics.com>
2017-06-02 15:51:01 +0200

    Digits and letters

--- LikeAProgrammer/Assets/scripts/Character.cs
+++ LikeAProgrammer/Assets/scripts/Character.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Character : Enemy {
 
 	private string value;
+	public Sprite[] sprites;
 
 	public string getValue() {
 
 		return value;
+	}
+
+	void Start() {
+		gameObject.GetComponent<SpriteRenderer> ().sprite = sprites[Random.Range(0, 26)];
 	}
 }
--- LikeAProgrammer/Assets/scripts/Integer.cs
+++ LikeAProgrammer/Assets/scripts/Integer.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Integer : Enemy {
 
 	private int value;
+	public Sprite[] sprites;
 
 	public int getValue() {
 
 		return value;
+	}
+
+	void Start() {
+		gameObject.GetComponent<SpriteRenderer> ().sprite = sprites[Random.Range(0, 10)];
 	}
 }
