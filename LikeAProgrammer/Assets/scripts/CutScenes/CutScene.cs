a37174f60f5b53ab5154c74de675a1e5b1b6b0d0
Milena Cakovic <milena.cakovic@namics.com>
2017-06-17 18:12:29 +0200

    Add cut scenes fading and create remaining dummy scenes

--- LikeAProgrammer/Assets/scripts/CutScenes/CutScene.cs
+++ LikeAProgrammer/Assets/scripts/CutScenes/CutScene.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CutScene : MonoBehaviour {
 
 	public int sceneDuration = 4;
 	public int nextScene = 2;
+
+	public Image black;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +19,8 @@ public class CutScene : MonoBehaviour {
 	
 	IEnumerator PlayScene() {
 		yield return new WaitForSecondsRealtime(sceneDuration);
+		anim.SetBool ("Fade", true);
+		yield return new WaitUntil(()=>black.color.a==1);
 		SceneManager.LoadScene(nextScene);
 	}
 }
